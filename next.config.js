d37dfd41ecc35fe774343e7b4b5a868d6da29b10
louisxie2018 <36788327+louisxie2018@users.noreply.github.com>
2025-04-25 11:38:04 -0400

    1

--- next.config.js
+++ next.config.js
@@ -14,3 +14,15 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+// next.config.js
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  // …your existing config…
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
+}
