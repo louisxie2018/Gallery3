c45f75252abbd398c337e183c3e577a684a6ac32
louisxie2018 <36788327+louisxie2018@users.noreply.github.com>
2025-04-25 16:43:57 -0400

    1

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
 import Intro from '../components/Intro';
-import Gallery from '../components/Gallery';
-import About from '../components/About';
 import Footer from '../components/Footer';
 
 export default function Home() {
